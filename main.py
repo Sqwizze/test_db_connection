bd08e7339ffdbdb27f28fb0fdb23d2dc195c3c71
stas <speckrus322@gmail.com>
2023-10-05 17:36:39 +0300

    create tables and columns NOT BD

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
 
 engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost/postgres", echo=True, pool_size=6)
 engine.connect()
+# print(engine)
 
-print(engine)
+metadata = MetaData()
+
+users = Table('users', metadata,
+              Column('id', Integer(), primary_key=True),
+              Column('username', String(15)),
+              Column('email', String(50))
+              )
+
+posts = Table('posts', metadata,
+              Column('user_id', ForeignKey('users.id'), primary_key=True),
+              Column('description', String(150))
+              )
+
+for _tables in metadata.tables:
+    print(metadata.tables[_tables])
+
+print(posts.columns)
+print(users.columns)
